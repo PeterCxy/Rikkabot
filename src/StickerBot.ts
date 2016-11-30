4750cce62550108801c18abbf3eab50b41dc8da6
Peter Cai <peter@typeblog.net>
2016-11-30 17:38:56 +0800

    StickerBot: allow testing
    Signed-off-by: Peter Cai <peter@typeblog.net>

--- src/StickerBot.ts
+++ src/StickerBot.ts
@@ -2,6 +2,7 @@ import * as fs from "fs"
 import { Telegram } from "./api/Telegram"
 
 const RIKKA = "RikkaW"
+const TESTER = process.env.RIKKA_BOT_DEBUG_USER
 const STAT_FILE = "./data/statistics.json"
 const SAVE_INTERVAL = 60 * 1000
 const THRESHOLD = 400
@@ -48,7 +49,7 @@ export namespace StickerBot {
     if (message.chat.id == message.from.id) {
       return // DO NOT accept private chats
     }
-    if (message.from.username === RIKKA) {
+    if (message.from.username === RIKKA || (TESTER != null && message.from.username === TESTER)) {
       if (message.sticker != null) {
         statistics.total++
         if (statistics.stickers[message.sticker.file_id] == null) {
